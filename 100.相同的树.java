d7041e3cacb903fccf0dcaef7e908e2d232dff61
tang_huibo <1215893264@qq.com>
2022-04-25 23:27:10 +0800

    100

--- 100.相同的树.java
+++ 100.相同的树.java
@@ -22,7 +22,19 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-
+        if(q == null && p == null) {
+            return true;
+        }
+        if(p == null && q != null) {
+            return false;
+        }
+        if(q == null && p != null) {
+            return false;
+        }
+        if(p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
 // @lc code=end
